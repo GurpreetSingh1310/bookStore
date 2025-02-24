f725ef3307badf2be529e1c0b1b25c444f2254e2
Gurpreet Singh <gurpreet.singh@affluencesolutions.co.in>
2025-02-25 02:35:40 +0530

    ``` refactor project structure for improved organization ```

--- src/main/java/com/bookstore/BookStore/repositories/BookRepository.java
+++ src/main/java/com/bookstore/BookStore/repositories/BookRepository.java
@@ -3,7 +3,18 @@ package com.bookstore.BookStore.repositories;
 import com.bookstore.BookStore.entities.Book;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface BookRepository extends ReactiveCrudRepository<Book, Integer> {
+
+    public Mono<Book> findByBookName(String bookName);
+
+    public Flux<Book> findByAuthor(String author);
+
+    public Flux<Book> findByPublisher(String publisher);
+
+    public Flux<Book> findByBookNameAndAuthor(String name, String author);
+
 }
--- src/test/java/com/bookstore/BookStore/service/RepoTest.java
+++ src/test/java/com/bookstore/BookStore/service/RepoTest.java
@@ -1,14 +1,61 @@
-package com.bookstore.BookStore;
+package com.bookstore.BookStore.service;
 
+import com.bookstore.BookStore.entities.Book;
+import com.bookstore.BookStore.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 public class RepoTest {
 
+    private Object BookRepository;
+
+    @org.junit.Test
     @Test
     public void testing() {
-        System.out.println("test");
+        System.out.println("testing method");
     }
+
+    Mono<Book> nameMono = BookRepository.findByName("Angular front end");
+//        StepVerifier.create(nameMono)
+//                .expectNextCount(1)
+//                .verifyComplete();
+
+//        Flux<Book> authorFlux = bookRepository.findByAuthor("ravi rampal");
+//        StepVerifier.create(authorFlux)
+//                .expectNextCount(2)
+//                .verifyComplete();
+
+        BookRepository.findByNameAndAuthor("Angular front end","rohit")
+                .
+
+    as(StepVerifier::create)
+                .
+
+    expectNextCount(1)
+                .
+
+    verifyComplete();
+
+
+    @Test
+    public void queryMethodsTest() {
+    //
+    //        bookRepository.getAllBooksByAuthor("Basic Python Book","Python rocker")
+    //                .as(StepVerifier::create)
+    //                .expectNextCount(1)
+    //                .verifyComplete()zzzz;
+
+        BookRepository.searchBookByTitle("%front%")
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+
+    }
+}
+
+
 }
 
