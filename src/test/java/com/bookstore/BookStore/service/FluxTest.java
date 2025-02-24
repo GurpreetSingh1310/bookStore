84ae263ea688b3efaa34ebe0f130ba7d7b42bc5b
Gurpreet Singh <gurpreet.singh@affluencesolutions.co.in>
2025-02-25 02:35:03 +0530

    add initial test classes for service layer

---
+++ src/test/java/com/bookstore/BookStore/service/FluxTest.java
@@ -0,0 +1,4 @@
+package com.bookstore.BookStore.service;
+
+public class FluxTest {
+}
---
+++ src/test/java/com/bookstore/BookStore/service/RepoTest.java
@@ -0,0 +1,14 @@
+package com.bookstore.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RepoTest {
+
+    @Test
+    public void testing() {
+        System.out.println("test");
+    }
+}
+
